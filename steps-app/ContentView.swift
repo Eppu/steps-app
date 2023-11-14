3ffa605ce76dce639e6474cdde1c3af0ce6a3124
Eetu Eskelinen <eetuesk@gmail.com>
2023-11-14 12:37:03 +0200

    Add HealthKit entitlement

--- steps-app/ContentView.swift
+++ steps-app/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    // add a counter property
+    @State private var counter = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            // Display the counter
+            Text("Counter: \(counter)")
+            
+            // Add a button that increments the counter
+            Button("Tap me!") {
+                counter += 1
+            }
         }
         .padding()
     }
