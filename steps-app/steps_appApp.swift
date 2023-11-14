6528854c99f46e202b9f74d3e3e4ca496ee6d207
Eetu Eskelinen <eetuesk@gmail.com>
2023-11-14 12:09:59 +0200

    Initial Commit

---
+++ steps-app/steps_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  steps_appApp.swift
+//  steps-app
+//
+//  Created by Eetu Eskelinen on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct steps_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
